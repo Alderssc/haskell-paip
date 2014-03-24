c52d31adb3b71f360af2a5876c52b2cd77cbcd83
Stefan Alderson <euler90h@gmail.com>
2014-03-24 14:11:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+HASKELL-PAIP
+============
+This repository holds code that I've written while reading Norvig's Paradigms of Artificial Intelligence Programming. I'm new to Haskell, so usage of this code for non-pedagogic reasons is inadvisable.
---
+++ grammar.hs
@@ -0,0 +1,45 @@
+import System.Random (randomRIO)
+type Verb = String
+type Noun = String
+type Article = String
+
+data NounPhrase = NP Article Noun 
+data VerbPhrase = VP Verb NounPhrase 
+data Sentence = Sentence NounPhrase VerbPhrase 
+
+verbs :: [Verb]
+verbs = ["hugs", "drops"]
+
+nouns :: [Noun]
+nouns = ["woman", "man"]
+
+articles :: [Article]
+articles = ["a", "the"]
+
+chooseR :: [a] -> IO a
+chooseR xs = randomRIO (0, length xs - 1) >>= \n -> return (xs !! n)
+
+genNP :: IO NounPhrase
+genNP = chooseR articles >>= \a -> 
+          chooseR nouns >>= \n ->
+            return $ NP a n
+
+genVP :: IO VerbPhrase
+genVP = chooseR verbs >>= \v ->
+          genNP >>= \np ->
+            return $ VP v np
+
+genSentence :: IO Sentence
+genSentence = genNP >>= \np ->
+                genVP >>= \vp ->
+                  return $ Sentence np vp
+
+instance Show NounPhrase where
+  show (NP a n) = a ++ " " ++ n 
+instance Show VerbPhrase where
+  show (VP v np) = v ++ " " ++ show np
+instance Show Sentence where
+  show (Sentence np vp) = show np ++ " " ++ show vp
+
+main :: IO ()
+main = genSentence >>= print
